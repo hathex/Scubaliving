bfc89f219cb3e69e742fcd3f85511cdb5078f800
hathex <50103127+hathex@users.noreply.github.com>
2019-10-03 12:42:57 +0200

    gh

---
+++ precache-manifest.f4816aef3f344a96406823df2247a130.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5543c6142384c4e2115007f088bddee2",
+    "url": "/scubaliving/index.html"
+  },
+  {
+    "revision": "ac29387c316dbc2bcfc3",
+    "url": "/scubaliving/static/css/main.07271905.chunk.css"
+  },
+  {
+    "revision": "d5e886d29e5434153aea",
+    "url": "/scubaliving/static/js/2.d6769305.chunk.js"
+  },
+  {
+    "revision": "ac29387c316dbc2bcfc3",
+    "url": "/scubaliving/static/js/main.80cbc224.chunk.js"
+  },
+  {
+    "revision": "6220d69063b9e95199ca",
+    "url": "/scubaliving/static/js/runtime-main.865e7a5f.js"
+  },
+  {
+    "revision": "f9b9e60ed0c5f09654223fc1ed85ab4a",
+    "url": "/scubaliving/static/media/1200px-Logo_of_PADI.svg.f9b9e60e.png"
+  },
+  {
+    "revision": "e071d9474af04abc95abe621ddcb6daf",
+    "url": "/scubaliving/static/media/ScubaLiving_logo_small.e071d947.svg"
+  },
+  {
+    "revision": "cd8b758bc21da9a448f5b1a959b23b94",
+    "url": "/scubaliving/static/media/scuba2.cd8b758b.jpg"
+  }
+]);
